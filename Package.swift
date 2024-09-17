1bb949612b8ea1a3cacd0a1001d890ede9ef3b9a
lahari <gplahari@gmail.com>
2024-09-17 15:28:08 +0200

    Update packages

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,10 +13,10 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-      .package(url: "https://github.com/mbarnach/SwiftyRequest", from: "3.1.1"),
+        .package(url: "https://github.com/StyleShoots/SwiftyRequest", branch: "master"),
 
-      // Testing dependencies
-         .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
+        // Testing dependencies
+        .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
